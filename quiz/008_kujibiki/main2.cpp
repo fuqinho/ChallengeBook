07a7902da96d8217cffedb7861c83e5e52f7fef8
fuqinho <kitakita7@gmail.com>
2011-09-28 22:26:18 +0900

    add comment

--- quiz/008_kujibiki/main2.cpp
+++ quiz/008_kujibiki/main2.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 string solve(int N, int M, vector<int>& K) {
+    // 2つのくじを引いた合計の数を列挙する
     vector<int> pair_totals;
     for (int i = 0; i < K.size(); i++)
         for (int j = 0; j < K.size(); j++)
             pair_totals.push_back(K[i] + K[j]);
+
+    // 二分探索のためにソートする
     sort(pair_totals.begin(), pair_totals.end());
 
+    // pair_totalsの中身から2つの数を取り出してMになれば良い。
+    // １つ取り出して、M-その数を二分探索で探す
     for (int i = 0; i < pair_totals.size(); i++)
         if (binary_search(pair_totals.begin(), pair_totals.end(),
                     M - pair_totals[i]))
