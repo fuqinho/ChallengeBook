ca59b38b1c3272806d5615f27e5fbb0ef6e8cbe6
Fukino Naoki <fuqinho@Fukinos-MacBook-Air.local>
2012-04-06 15:48:53 +0900

    solved 186

--- quiz2nd/186_MinimizingMaximizer/main2.cpp
+++ quiz2nd/186_MinimizingMaximizer/main2.cpp
@@ -17,6 +17,7 @@
 #include <cmath>
 #include <cstring>
 #include <string>
+#include <climits>
 using namespace std;
 
 typedef vector<int> VI;
@@ -38,6 +39,50 @@ std::ostream& operator<<(std::ostream& stream, const std::pair<T1, T2>& data) {
   return stream << "(" << data.first << "," << data.second << ")";
 }
 
+static const int INF = 100000000;
+
+class RMQ {
+public:
+
+  RMQ(int n_) {
+    local_size = 1;
+    while (local_size < n_) local_size *= 2;
+    minimum = vector<int>(2 * local_size - 1);
+    for (int i = 0; i < 2 * local_size - 1; i++) minimum[i] = INF;
+  }
+
+  // update element at k by a
+  void update(int k, int a) {
+    k += local_size - 1;
+    minimum[k] = a;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      minimum[k] = min(minimum[k*2+1], minimum[k*2+2]);
+    }
+  }
+
+  // get minimum element in [a, b)
+  int query(int a, int b) {
+    return query_(a, b, 0, 0, local_size);
+  }
+
+private:
+  int local_size;
+  vector<int> minimum;
+
+  int query_(int a, int b, int k, int l, int r) {
+    if (r <= a || b <= l) return INF;
+
+    if (a <= l && b >= r) {
+      return minimum[k];
+    } else {
+      int vl = query_(a, b, k*2+1, l, (l+r)/2);
+      int vr = query_(a, b, k*2+2, (l+r)/2, r);
+      return min(vl, vr);
+    }
+  }
+};
+
 int main() {
   int n, m;
   scanf("%d %d", &n, &m);
@@ -48,25 +93,20 @@ int main() {
   }
  
   // dp[i][j]: 
-  // i番目のSorterまでで、最大値がj番目に移動するような最小の部分列の長さ
-  static const int INF = 100000000;
   VI dp(n+1, INF);
+  RMQ rmq(n+1);
 
-  dp[1] = 0;
+  //dp[1] = 0;
+  rmq.update(0, 0);
   REP(i, m) {
     int s = sorters[i].first;
     int t = sorters[i].second;
 
-    //cerr << "s: " << s << ", " << "t: " << t << endl;
-    
-    int minLength = INF;
-    for (int j = s; j <= t; j++) {
-      minLength = min(minLength, dp[j]);
-    }
-    dp[t] = min(minLength + 1, dp[t]);
-
+    int minLength = rmq.query(s-1, t);
+    //dp[t] = min(minLength + 1, dp[t]);
+    int next = min(minLength + 1, rmq.query(t-1, t));
+    rmq.update(t-1, next);
   }
-  cout << dp[n] << endl;
-
+  cout << rmq.query(n-1, n) << endl;
 }
 
